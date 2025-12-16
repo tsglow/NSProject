0d884c18c7ef32372c2e9582eebb375d98ac61bf
dinner_rolls <haruki84@msn.com>
2025-12-16 09:27:17 +0900

    first commit

---
+++ HSNS/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for HSNS project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HSNS.settings')
+
+application = get_asgi_application()
